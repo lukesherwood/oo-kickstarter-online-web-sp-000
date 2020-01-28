3e1874903999f6f37bb25a9395808d00b5404920
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 18:27:38 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Project
+  attr_accessor :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+    
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end
+  
+  
+end
