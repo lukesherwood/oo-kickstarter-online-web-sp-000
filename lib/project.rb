057a56d578cdadbd8f3a3ca0cdbcdc4a762491f0
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 18:31:48 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Project
+  attr_accessor :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+    
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  
+  end
+  
+  
+end
